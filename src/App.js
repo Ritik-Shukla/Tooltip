6cce1263e50d726956d6fc15462d2bb13b5ff055
Ritik <shuklaritik731@gmail.com>
2024-07-09 16:56:37 +0530

    adding files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import ToolPit from './components/tooltip';
+
 
 function App() {
+  const [selectedButton, setSelectedButton] = useState('top');
+  const [position, setPosition] = useState('top');
+
+  //function called when position button is clicked
+
+  function handleButtonClick(event) {
+    //getting the position of the toolpit from the buttons
+    setPosition(prevPostion => {
+      return prevPostion = event.target.value;
+    })
+    //making the button selected
+    setSelectedButton(event.target.value);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>ToolTip</h2>
+      <div className='button-container'>
+        <button onClick={handleButtonClick} value="left" className={selectedButton === "left" ? "selected" : ""}>Left</button>
+        <button onClick={handleButtonClick} value="right" className={selectedButton === "right" ? "selected" : ""}>Right</button>
+        <button onClick={handleButtonClick} value="top" className={selectedButton === "top" ? "selected" : ""}>Top</button>
+        <button onClick={handleButtonClick} value="bottom" className={selectedButton === "bottom" ? "selected" : ""}>Bottom</button>
+      </div>
+      <ToolPit position={position} />
     </div>
   );
 }
